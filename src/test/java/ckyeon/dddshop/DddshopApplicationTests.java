168cc96ccf0c189e2e98dbdadac7b3311e6f4dc5
ckyeon <qsky8520@gmail.com>
2022-08-17 23:56:41 +0900

    initial commit

---
+++ src/test/java/ckyeon/dddshop/DddshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ckyeon.dddshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
