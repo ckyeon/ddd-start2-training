68edd2a2bd77e87f1930c745dfe535dd55e7f67c
ckyeon <qsky8520@gmail.com>
2022-08-18 01:09:27 +0900

    Fix Order, OrderState

--- src/main/java/ckyeon/dddshop/order/domain/Order.java
+++ src/main/java/ckyeon/dddshop/order/domain/Order.java
@@ -1,13 +1,19 @@
 package ckyeon.dddshop.order.domain;
 
 public class Order {
+
 	private OrderState state;
 	private ShippingInfo shippingInfo;
 
 	public void changeShippingInfo(ShippingInfo newShippingInfo) {
-		if (!state.isShippingChangeable()) {
+		if (!isShippingChangeable()) {
 			throw new IllegalStateException("can't change shipping in " + state);
 		}
 		this.shippingInfo = newShippingInfo;
+	}
+
+	private boolean isShippingChangeable() {
+		return state == OrderState.PAYMENT_WAITING ||
+			state == OrderState.PREPARING;
 	}
 }
